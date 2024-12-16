315700e8a49771b1d4496c00961778280f869bad
abishaVadivelu <abishavadivelu@gmail.com>
2024-12-16 22:34:39 +0530

    Add files via upload
    Day 1 Assignment

---
+++ day1/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  float Version = 91.0f;
+		  String developer = "Google";
+		  boolean Beta = false;
+		  int releaseYear =2008;
+		  char shortcutKey ='C';
+		  System.out.println("Type of Version:" +Version);
+		  System.out.println("Type of Developer:" +developer);
+		  System.out.println("Type of Beta:" +Beta +"\n" + "Released Year:" + releaseYear);
+		  System.out.println("Type of shortcutKey:" + shortcutKey);
+
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  float browserVersion = 100.2f;
+  String browserName = "firefox";
+  boolean isVisible = true;
+  int releaseYear =1998;
+  char browserLogo ='f';
+  
+  System.out.println("Browser Name: "+browserName);
+  System.out.println("Browser Version: "+browserVersion +"\n" +"Release Year:"+releaseYear);
+  System.out.println("Browser Logo:" +browserLogo + "\n" +"IsVisible:" +isVisible);
+	}
+
+}
